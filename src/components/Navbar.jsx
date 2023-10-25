ae04555c5bb0d4d5f677c7101671cebdd34eb927
prathik-p <prathik.prejith@gmail.com>
2023-10-26 00:25:05 +0530

    Main homepage

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -3,28 +3,36 @@ import "../styles/Navbar.scss";
 function Navbar() {
   return (
     <>
-      <div className="navbar">
-        <div className="nav-left">ARTICON</div>
-        <div className="nav-center"></div>
-        <div className="nav-right">
-          <a>About</a>
-          <a>News</a>
-          <a>Team</a>
+      <div className="navbar-content-1">
+        <div className="nav-center">
+          <a>ABOUT</a>
+          <a>TEAMS</a>
+          <a>EVENTS</a>
         </div>
       </div>
     </>
   );
 }
 
-function EventsNavbar() {
+function TeamNavbar() {
   return (
     <>
-      <div className="navbar">
-        <div className="nav-left">Upcoming Events</div>
+      <div className="navbar-content-4">
+        <div className="nav-left">MEET THE TEAM</div>
+      </div>
+    </>
+  );
+}
+
+function PastEventsNavbar() {
+  return (
+    <>
+      <div className="navbar-content-3">
+        <div className="nav-left">PAST EVENTS</div>
       </div>
     </>
   );
 }
 
 export default Navbar;
-export { EventsNavbar };
+export { TeamNavbar, PastEventsNavbar };
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,26 +1,52 @@
-import Navbar, { EventsNavbar } from "../components/Navbar";
+import Navbar, { TeamNavbar, PastEventsNavbar } from "../components/Navbar";
 import "../styles/Home.scss";
 
 function Home() {
   return (
     <>
-      <div>
-        <Navbar />
-      </div>
-      <div className="content-1">
-        <div className="aids-bar"></div>
-        <div className="aids">
-          DEPARTMENT OF <br />
-          ARTIFICIAL INTELLIGENCE <br />
-          AND DATA SCIENCE
+      <div className="header">
+        <div className="black-line1"></div>
+        <div className="navbar-container">
+          <div className="black-line2"></div>
+          <Navbar />
+          <div className="black-line2"></div>
         </div>
       </div>
-      <div className="footer">
+      <div className="content-1">
+        <div className="brain-img-container">
+          <div className="brain-img"></div>
+        </div>
+        <div className="line-content">
+          <div className="black-line3"></div>
+          <div className="black-line4"> </div>
+          <div className="black-line5"></div>
+        </div>
+        <div className="text-content">
+          <div className="aids">
+            DEPARTMENT <br />
+            OF ARTIFICIAL <br />
+            INTELLIGENCE
+            <br />
+            AND DATASCIENCE
+          </div>
+          <div className="dept-aids">
+            Official department association
+            <br />
+            for Artificial Intelligence
+            <br />
+            and Data Science
+          </div>
+          <div className="rset-image-container">
+            <div className="rset-img"></div>
+          </div>
+        </div>
+      </div>
+      <div className="footer-content-1">
         <ul className="articon">
-          <a className="a">A</a>
+          <a>A</a>
           <a>R</a>
           <a>T</a>
-          <a className="i">I</a>
+          <a>I</a>
           <a>C</a>
           <a>O</a>
           <a>N</a>
@@ -28,30 +54,102 @@ function Home() {
       </div>
       {/* #endregion */}
       <div className="content-2">
-        <div className="about-text">
-          Esse labore eiusmod voluptate minim elit dolor nulla ullamco. Ex eu
-          <br />
-          velit mollit ad magna ut ipsum dolor aute occaecat tempor nulla.
-          Mollit
-          <br />
-          irure incididunt deserunt adipisicing ullamco. Sit in mollit labore
-          <br />
-          laboris adipisicing ea sunt excepteur Lorem exercitation ea eu ea
-          nulla.
-          <br />
-          Aliqua sint magna nulla ullamco cupidatat consectetur fugiat ex eu
-          <br />
-          mollit.
-        </div>
+        Esse labore eiusmod voluptate minim elit dolor nulla ullamco. Ex eu
+        <br />
+        velit mollit ad magna ut ipsum dolor aute occaecat tempor nulla. Mollit
+        <br />
+        irure incididunt deserunt adipisicing ullamco. Sit in mollit labore
+        <br />
+        laboris adipisicing ea sunt excepteur Lorem exercitation ea eu ea nulla.
+        <br />
+        Aliqua sint magna nulla ullamco cupidatat consectetur fugiat ex eu
+        <br />
+        mollit.
       </div>
       {/* #endregion */}
-      <EventsNavbar />
+      <PastEventsNavbar />
       <div className="content-3">
         <div className="events">
           <div className="grid-container">
-            <div className="grid-item">Hello</div>
-            <div className="grid-item">Hello</div>
-            <div className="grid-item">Hello</div>
+            <div className="grid-item">
+              <div className="details">
+                <div className="inaug">
+                  ARTICON
+                  <br />
+                  INNAUGRATION
+                </div>
+                <div className="date">22 OCT 2023</div>
+              </div>
+            </div>
+            <div className="grid-item">
+              <div className="details">
+                <div className="inaug">
+                  ARTICON
+                  <br />
+                  INNAUGRATION
+                </div>
+                <div className="date">22 OCT 2023</div>
+              </div>
+            </div>
+            <div className="grid-item">
+              <div className="details">
+                <div className="inaug">
+                  ARTICON
+                  <br />
+                  INNAUGRATION
+                </div>
+                <div className="date">22 OCT 2023</div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+      {/* #endregion */}
+      <TeamNavbar />
+      <div className="content-4">
+        <div className="teams">
+          <div className="grid-container">
+            <div className="grid-item-1">
+              <div className="details">
+                <div className="name">John Doe</div>
+                <div className="work">Developer</div>
+                <div className="insta"></div>
+                <div className="linked"></div>
+              </div>
+            </div>
+            <div className="grid-item-1">
+              <div className="details">
+                <div className="name">John Doe</div>
+                <div className="work">Developer</div>
+                <div className="insta"></div>
+                <div className="linked"></div>
+              </div>
+            </div>
+            <div className="grid-item-1">
+              <div className="details">
+                <div className="name">John Doe</div>
+                <div className="work">Developer</div>
+                <div className="insta"></div>
+                <div className="linked"></div>
+              </div>
+            </div>
+            <div className="grid-item-2">
+              <div className="discover">
+                Discover
+                <br />
+                the full
+                <br />
+                Team
+              </div>
+              <div className="more-teams">
+                <div className="absolute-content">
+                  <div className="teams-text">Team</div>
+                  <div className="teams-button">
+                    <button>{">"}</button>
+                  </div>
+                </div>
+              </div>
+            </div>
           </div>
         </div>
       </div>
